2be37cda6f580180342e58a68c8147a57bef9622
razibulislam014 <razibulislam665@gmail.com>
2026-04-13 07:36:09 +0600

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Heading from "./components/Heading";
+import Section from "./components/Section";
+
+function App() {
+  return (
+    <Section>
+      <Heading level={1}>Title</Heading>
+      <Heading level={2}>Heading</Heading>
+      <Heading level={3}>Sub-heading</Heading>
+      <Heading level={4}>Sub-sub-heading</Heading>
+      <Heading level={5}>Sub-sub-sub-heading</Heading>
+      <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+    </Section>
+  );
+}
+
+export default App;
